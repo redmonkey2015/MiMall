0aba79d70bc0371f65589bae034845b2d839ddda
redmonkey2015 <redmonkey@qq.com>
2020-06-30 18:56:03 +0800

    封装storage

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,14 @@
     <router-view/>
   </div>
 </template>
+<script>
+import storage from './storage'
+export default {
+  name: 'app',
+  mounted() {
+    //storage.setItem('key1','value1')
+    //storage.setItem('user',{name: 'pc'})
+    storage.setItem('user', {name: 'pc'}, 'passport')
+  }
+}
+</script>
