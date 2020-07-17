32215721434391f69dece5b65ccc926c6306b627
redmonkey2015 <redmonkey@qq.com>
2020-07-17 15:30:52 +0800

    订单确认

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -2,17 +2,20 @@
     <div>
         <order-header></order-header>
         <router-view></router-view>
+        <service-bar></service-bar>
         <nav-footer></nav-footer>
     </div>
 </template>
 <script>
     import OrderHeader from '../components/OrderHeader'
     import NavFooter from '../components/NavFooter'
+    import ServiceBar from '../components/ServiceBar'
     export default {
         name: 'order',
         components: {
             OrderHeader,
-            NavFooter
+            NavFooter,
+            ServiceBar
         }
     }
 </script>
