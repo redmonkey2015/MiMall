a7a36c04267412e4c0cfa86fae494509c8370a63
redmonkey2015 <redmonkey@qq.com>
2020-07-17 17:08:57 +0800

    支付页面

--- src/main.js
+++ src/main.js
@@ -1,51 +1,53 @@
 import Vue from 'vue'
 import router from './router'
-import store from './store'
-import axios from 'axios';
+import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueLazyLoad from 'vue-lazyload'
-import App from './App.vue'
 import VueCookie from 'vue-cookie'
 import { Message } from 'element-ui'
-//import evn from './env.js'
-
-const mock = false
-if (mock) {
-  require('./mock/api')
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import App from './App.vue'
+// import env from './env'
+// mock开关
+const mock = false;
+if(mock){
+  require('./mock/api');
 }
-axios.defaults.baseURL = '/api'
-axios.defaults.timeout = 8000
-
-//axios.defaults.baseURL = evn.baseURL
-
-axios.interceptors.response.use(function(response) {
-  let returnData = response.data
-  let path = location.hash
-
-  if (returnData.status == 0) {
-    return returnData.data
-  } else if (returnData.status == 10) {
-    if (path != '/#/login') {
-      window.location.href = '/#/login'
-    }
-    return Promise.reject(returnData)
-  } else {
-    alert(returnData.msg)
-    return Promise.reject(returnData)
+// 根据前端的跨域方式做调整 /a/b : /api/a/b => /a/b
+// axios.defaults.baseURL = 'https://www.easy-mock.com/mock/5dc7afee2b69d9223b633cbb/mimall';
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000; 
+// 根据环境变量获取不同的请求地址
+// axios.defaults.baseURL = env.baseURL;
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if(res.status == 0){
+    return res.data;
+  }else if(res.status == 10){
+    window.location.href = '/#/login';
+    return Promise.reject(res);
+  }else{
+    Message.warning(res.msg);
+    return Promise.reject(res);
   }
-})
+},(error)=>{
+  let res = error.response;
+  Message.error(res.data.message);
+  return Promise.reject(error);
+});
 
-Vue.use(VueCookie)
-Vue.use(VueAxios, axios)
-Vue.use(VueLazyLoad, {
-  loading: '/imgs/loading-svg/loading-bars.svg'
+Vue.use(VueAxios,axios);
+Vue.use(VueCookie);
+Vue.use(VueLazyLoad,{
+  loading:'/imgs/loading-svg/loading-bars.svg'
 })
-
 Vue.prototype.$message = Message;
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   store,
-  render: h => h(App)
+  router,
+  render: h => h(App),
 }).$mount('#app')
